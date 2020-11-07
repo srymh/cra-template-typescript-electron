4bd88a7f929efbef89d0ee96e33aad7e6a97dd66
srymh <31023018+srymh@users.noreply.github.com>
2020-11-08 00:42:04 +0900

    electronize

--- template/src/App.tsx
+++ template/src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const myIpcRenderer = window.myIpcRenderer;
+
 function App() {
   return (
     <div className="App">
@@ -14,10 +16,30 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
+        <button
+          style={{
+            color: '#61dafb',
+            backgroundColor: '#282c34',
+            border: '2px solid white',
+            borderRadius: '3px 3px',
+            marginTop: '50px',
+            cursor: 'pointer',
+          }}
+          onClick={() => {
+            myIpcRenderer
+              .invoke('APP_hello', 'ping')
+              .then((message) => {
+                alert(`Received "${message}" from the main process`);
+              })
+              .catch((error) => {
+                alert(error);
+              });
+          }}>
+          Send "ping" to a main process
+        </button>
       </header>
     </div>
   );
